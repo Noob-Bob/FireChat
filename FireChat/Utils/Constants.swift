f0281a167bdd85fc6e9e9bfa06f312a07d755df9
Xuecheng Zhang <105913432+Noob-Bob@users.noreply.github.com>
2023-12-03 15:31:37 -0800

    upload message to firestore
    copied the same message and stored in both sender and receiver

---
+++ FireChat/Utils/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  FireChat
+//
+//  Created by Xuecheng Zhang on 12/3/23.
+//
+
+import FirebaseFirestore
+
+let COLLECTION_MESSAGES = Firestore.firestore().collection("messages")
